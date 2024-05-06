e59ed4a6cefdb4def6a8add0bbb36a9982964213
Aditya Maurya <madityauma20@gmail.com>
2024-05-06 22:40:44 +0530

    first commit

---
+++ 1. Getting Started/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d
@@ -0,0 +1,5 @@
+/home/odoo/Rust/Tutorials/1. Getting Started/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42: src/main.rs
+
+/home/odoo/Rust/Tutorials/1. Getting Started/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d: src/main.rs
+
+src/main.rs:
---
+++ 1. Getting Started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/odoo/Rust/Tutorials/1.\ Getting\ Started/hello_cargo/target/debug/hello_cargo: /home/odoo/Rust/Tutorials/1.\ Getting\ Started/hello_cargo/src/main.rs
